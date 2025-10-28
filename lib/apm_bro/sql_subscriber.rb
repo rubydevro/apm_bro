46eecd67d1fd17071e28f5f376fb618e1c5620f1
Emanuel Comsa <cremzinc@gmail.com>
2025-10-28 14:24:41 +0100

    track sql allocations

--- lib/apm_bro/sql_subscriber.rb
+++ lib/apm_bro/sql_subscriber.rb
@@ -7,11 +7,28 @@ module ApmBro
   class SqlSubscriber
     SQL_EVENT_NAME = "sql.active_record".freeze
     THREAD_LOCAL_KEY = :apm_bro_sql_queries
+    THREAD_LOCAL_ALLOC_START_KEY = :apm_bro_sql_alloc_start
+    THREAD_LOCAL_ALLOC_RESULTS_KEY = :apm_bro_sql_alloc_results
 
     def self.subscribe!
+      # Subscribe with a start/finish listener to measure allocations per query
+      if ActiveSupport::Notifications.notifier.respond_to?(:subscribe)
+        begin
+          ActiveSupport::Notifications.notifier.subscribe(SQL_EVENT_NAME, SqlAllocListener.new)
+        rescue StandardError
+        end
+      end
+
       ActiveSupport::Notifications.subscribe(SQL_EVENT_NAME) do |name, started, finished, _unique_id, data|
         # Only track queries that are part of the current request
         next unless Thread.current[THREAD_LOCAL_KEY]
+        unique_id = _unique_id
+        allocations = nil
+        begin
+          alloc_results = Thread.current[THREAD_LOCAL_ALLOC_RESULTS_KEY]
+          allocations = alloc_results && alloc_results.delete(unique_id)
+        rescue StandardError
+        end
 
         query_info = {
           sql: sanitize_sql(data[:sql]),
@@ -19,7 +36,8 @@ module ApmBro
           duration_ms: ((finished - started) * 1000.0).round(2),
           cached: data[:cached] || false,
           connection_id: data[:connection_id],
-          trace: safe_query_trace(data)
+          trace: safe_query_trace(data),
+          allocations: allocations
         }
         # Add to thread-local storage
         Thread.current[THREAD_LOCAL_KEY] << query_info
@@ -29,11 +47,15 @@ module ApmBro
 
     def self.start_request_tracking
       Thread.current[THREAD_LOCAL_KEY] = []
+      Thread.current[THREAD_LOCAL_ALLOC_START_KEY] = {}
+      Thread.current[THREAD_LOCAL_ALLOC_RESULTS_KEY] = {}
     end
 
     def self.stop_request_tracking
       queries = Thread.current[THREAD_LOCAL_KEY]
       Thread.current[THREAD_LOCAL_KEY] = nil
+      Thread.current[THREAD_LOCAL_ALLOC_START_KEY] = nil
+      Thread.current[THREAD_LOCAL_ALLOC_RESULTS_KEY] = nil
       queries || []
     end
 
@@ -67,7 +89,7 @@ module ApmBro
       # Always try to get the full call stack for better trace information
       begin
         # Get the current call stack, skip the first few frames (our own code)
-        caller_stack = caller(3, 1) # Skip 3 frames, get up to 1
+        caller_stack = caller(3, 0) # Skip 3 frames, get up to 1
         caller_trace = caller_stack.map do |line|
           # Remove any potential sensitive information from file paths
           line.gsub(/\/[^\/]*(password|secret|key|token)[^\/]*\//i, '/[FILTERED]/')
@@ -106,5 +128,34 @@ module ApmBro
       []
     end
 
+  end
+end
+
+module ApmBro
+  # Listener that records GC allocation deltas per SQL event id
+  class SqlAllocListener
+    def start(name, id, payload)
+      begin
+        map = (Thread.current[ApmBro::SqlSubscriber::THREAD_LOCAL_ALLOC_START_KEY] ||= {})
+        map[id] = GC.stat[:total_allocated_objects] if defined?(GC) && GC.respond_to?(:stat)
+      rescue StandardError
+      end
+    end
+
+    def finish(name, id, payload)
+      begin
+        start_map = Thread.current[ApmBro::SqlSubscriber::THREAD_LOCAL_ALLOC_START_KEY]
+        return unless start_map && start_map.key?(id)
+
+        start_count = start_map.delete(id)
+        end_count = (GC.stat[:total_allocated_objects] rescue nil)
+        return unless start_count && end_count
+
+        delta = end_count - start_count
+        results = (Thread.current[ApmBro::SqlSubscriber::THREAD_LOCAL_ALLOC_RESULTS_KEY] ||= {})
+        results[id] = delta
+      rescue StandardError
+      end
+    end
   end
 end
